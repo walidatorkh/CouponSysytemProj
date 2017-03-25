230938c774b649f463cd82ad16501d0d07007278
walidatorkh <walidatorkh@gmail.com>
2017-03-25 14:55:02 +0300

    added in main System.out.println("start test");
    Change-Id: I0c9afea3619b35557b585c425d09d23ec686f010
    Signed-off-by: walidatorkh <walidatorkh@gmail.com>

--- src/t/TestC/Main.java
+++ src/t/TestC/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
 	public static void main(String[] args) throws RuntimeException {
 		try {
+		System.out.println("!!JOPA Started!!!");
 			Test.testCs();
 		} catch (Throwable e) {
 			// TODO Auto-generated catch block
